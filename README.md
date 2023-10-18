75fe0d7575895cd95a1af36a8ce6260bf081cac4
yousef fathy <yf1102003@gmail.com>
2023-10-18 05:30:59 +0300

    final touch

--- README.md
+++
@@ -1,1 +0,0 @@
-# nodejs-final-project
--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-require('dotenv').config();
-require('express-async-errors');
+require("dotenv").config();
+require("express-async-errors");
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const connectDB = require('./db/connect');
-const productsRouter = require('./routes/products');
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
-const notFoundMiddleware = require('./middleware/not-found');
-const errorMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
 
-// middleware
 app.use(express.json());
 
-// routes
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
 });
 
-app.use('/api/v1/products', productsRouter);
-
-// products route
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
